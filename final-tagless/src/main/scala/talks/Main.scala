f147af2ddb382910410ef8e9d394a7f52e8dcd68
Octav Zaharia <ozaharia@wehkamp.nl>
2018-08-09 13:26:57 +0300

    wip4

--- final-tagless/src/main/scala/talks/Main.scala
+++ final-tagless/src/main/scala/talks/Main.scala
@@ -16,7 +16,9 @@ trait LoggingOps[M[_]] {
   def error(message: String, t: Throwable): M[Unit]
 }
 
-class UserService[M[_] : Monad](implicit userOps: UserOps[M], logger: LoggingOps[M]) {
+import cats.~>
+
+class UserService[M[_] : Monad, F[_] : Monad](implicit userOps: UserOps[M], logger: LoggingOps[F], t: F ~> M) {
 
   import cats.implicits._
 
@@ -26,15 +28,13 @@ class UserService[M[_] : Monad](implicit userOps: UserOps[M], logger: LoggingOps
         if (user.pass == pass) {
           userOps.createSession(user)
             .flatMap { ret =>
-              logger.info("Created session").map(_ => Right(ret))
+              t(logger.info("Created session").map(_ => Right(ret)))
             }
         } else {
-          logger.error("Sorry", new Exception("Password is bad"))
-            .map(_ => Left(PasswordsNotMatch))
+          t(logger.error("Sorry", new Exception("Password is bad")).map(_ => Left(PasswordsNotMatch)))
         }
       case _          =>
-        logger.error("Sorry again", new Exception("User not found"))
-          .map(_ => Left(UserNotFound))
+        t(logger.error("Sorry again", new Exception("User not found")).map(_ => Left(UserNotFound)))
     }
 }
 
@@ -46,14 +46,16 @@ object Main extends App {
   import talks.Implicits._
   import cats.implicits._
 
-  val userServiceF = new UserService[Future]
-  val userServiceI = new UserService[Id]
+  implicit val transIdToFuture = new (Id ~> Future){
+    override def apply[A](fa: Id[A]) = Future.successful(fa)
+  }
+
+  val userServiceF = new UserService[Future, Id]
+
+
 
   println(userServiceF.login("email", "pass"))
   println(userServiceF.login("email1", "pass"))
 
-  println("*" * 20)
-  println(userServiceI.login("email", "pass"))
-  println(userServiceI.login("email1", "pass"))
 
 }
